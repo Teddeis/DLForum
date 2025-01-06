2365e13c28ac23d88c3e3d47dac559f9be736d69
Эрик Лейнинг <yarik13200566@mail.ru>
2025-01-06 05:34:50 +0400

    Изменение элементов в темной теме, исправление основных багов, а также улучшение дизайна

--- Service/TopicService.cs
+++ Service/TopicService.cs
@@ -1,12 +1,5 @@
-﻿using DLForum.Models;
-using DLForum.Service;
-using Microsoft.AspNetCore.Mvc;
-using Supabase;
-using Supabase.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Supabase;
+
 
 public class TopicService
 {
